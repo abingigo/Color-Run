9b5c6d76c15dba222ae3da36802e420f627a57ea
playerzero <abingigo@gmail.com>
2023-05-23 23:02:06 +0530

    Final Project

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public Text score;
+    public Text highScore;
+    public Text s;
+
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+    public void SetScore()
+    {
+        if(s.GetComponent<Score>().x > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", s.GetComponent<Score>().x);
+            highScore.text = s.GetComponent<Score>().x.ToString();
+        }
+        score.text = s.GetComponent<Score>().x.ToString();
+    }
+}
---
+++ Assets/Scripts/TimedObjectDestructor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TimedObjectDestructor : MonoBehaviour {
+
+	public GameObject player;
+
+	void FixedUpdate ()
+	{
+		if (transform.position.y > 2f)
+		{
+			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			if (GameObject.Find("Player").GetComponent<Renderer>().sharedMaterial != gameObject.GetComponent<Renderer>().sharedMaterial)
+				GetComponent<BoxCollider>().isTrigger = false;
+			else
+				GetComponent<BoxCollider>().isTrigger = true;
+			transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+		}
+		if (player.transform.position.x > transform.position.x)
+			DestroyNow();
+	}
+
+	void DestroyNow ()
+	{
+		Destroy(gameObject);
+	}
+}
